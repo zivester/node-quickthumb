6ad06bd3b93ea9a74a29f9b74790f7b5187d29b2
Zach Ivester <zivester@appnexus.com>
2014-04-25 22:45:47 -0400

    Fix file spacing

--- test/simple-test.js
+++ test/simple-test.js
@@ -4,33 +4,30 @@ var assert = require('assert'),
     im = require('imagemagick'),
     qt = require('../');
 
-(function(){
-    var ppath = path.normalize(__dirname + '/../public/images'),
-        src = path.join(ppath, 'cape cod.jpg');
+var ppath = path.normalize(__dirname + '/../public/images'),
+    src = path.join(ppath, 'cape cod.jpg');
 
-    var sizes = [
-        { width: 100, height: 100},
-        { width: 100, height: 50},
-    ];
+var sizes = [
+    { width: 100, height: 100},
+    { width: 100, height: 50},
+];
 
-    sizes.forEach(function(options){
-        var opt = {
-            src : src,
-            dst : path.join(ppath, 'red_' + options.width + 'x' + options.height + '.gif'),
-            width : options.width,
-            height : options.height,
-            quality : 1,
-        };
-        qt.convert( opt, function(err, image){
+sizes.forEach(function(options){
+    var opt = {
+        src : src,
+        dst : path.join(ppath, 'red_' + options.width + 'x' + options.height + '.gif'),
+        width : options.width,
+        height : options.height,
+        quality : 1,
+    };
+    qt.convert( opt, function(err, image){
+        assert.ifError(err);
+        assert.equal(image, opt.dst);
+        im.identify(image, function(err, features){
             assert.ifError(err);
-            assert.equal(image, opt.dst);
-            im.identify(image, function(err, features){
-                assert.ifError(err);
-                assert.equal(features.width, opt.width);
-                assert.equal(features.height, opt.height);
-                assert.ifError(fs.unlinkSync(image));
-            });
+            assert.equal(features.width, opt.width);
+            assert.equal(features.height, opt.height);
+            assert.ifError(fs.unlinkSync(image));
         });
     });
-
-})();
+});
