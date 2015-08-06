0912f695a38fb1853ffd605640ee69634955d16e
Christian Volmering <christian@volmering.name>
2015-08-06 21:19:32 +0200

    Added test case for image cache updates
    Added express to the devDependencies since it's used in the test cases.
    Added a modified image for testing the image cache update.
    Added test case to see if the cached version of a modified file is
    recreated.
    Added a missing return to the library.

--- test/express-test.js
+++ test/express-test.js
@@ -1,16 +1,23 @@
 var express = require('express'),
-    app = express(),
-    qt = require('../'),
-    filename = 'cape cod.jpg';
+    app     = express(),
+    qt      = require('../'),
+    os      = require('os'),
+    path    = require('path'),
+    fs      = require('fs'),
+
+    filename  = 'cape cod.jpg',
+    publicDir = path.join(__dirname, '../public'),
+    imageDir  = path.join(publicDir, 'images')
+    cacheDir  = path.join(os.tmpdir(), 'quickthumb/cache');
 
 
 // Crop
-app.use('/public/crop', qt.static(__dirname + '/../public', {
-    cacheDir : '/tmp/cache',
+app.use('/public/crop', qt.static(publicDir, {
+    cacheDir : cacheDir,
     quality : .95
 }));
 // Resize
-app.use('/public/resize', qt.static(__dirname + '/../public', {
+app.use('/public/resize', qt.static(publicDir, {
     type : 'resize',
 }));
 
@@ -31,10 +38,48 @@ app.get('/', function(req, res){
         });
         h += '<br />' + img(type, '?dim=800x100') + '<br />';
     });
-    h += '<br />original<br />' + img('crop','') + '</center>';
+    h += '<br />original<br />' + img('crop','') + '<br />';
+
+    // --- Image cache test 
+    h += '<br />cache test<br /><img src="/test/1" id="cacheTest" /><br />'
+      +  '<br /><input type="button" onclick="next()" value="Modify"/>'
+      +  '<script>function next(){document.getElementById("cacheTest").src="/test/"+ ++t}var t=1;</script>';
+
+    h += '</center>';
+
     res.send(h);
 });
 
+// --- Image cache test
+var cacheTestImage = 'cache_test.jpg';
+var modifiedImage  = 'cape cod modified.jpg';
+var redirectPath   = '/public/crop/images/' + cacheTestImage
+                   + '?dim=x200&' + new Date().getTime();
+
+app.get('/test/:step', function(req, res) {
+    switch (req.params.step) {
+        case '1': // Step 1: No cache, image has to be generated
+
+            // Create image for cache test
+            fs.createReadStream(path.join(imageDir, filename)).pipe(
+                fs.createWriteStream(path.join(imageDir, cacheTestImage)));
+
+            // Cached version is deleted so that it has to be generated
+            fs.unlink(path.join(cacheDir, 'crop/x200/images', cacheTestImage),
+                function(){/* ignore errors */});
+            break;
+
+        case '2': // Step 2: Image has to be regenerated since it was modified
+
+            // File is modifed so the cached version should be updated
+            fs.createReadStream(path.join(imageDir, modifiedImage)).pipe(
+                fs.createWriteStream(path.join(imageDir, cacheTestImage)));
+            break;
+
+    }
+
+    res.redirect(redirectPath);
+});
 
 app.listen(3000);
 console.log("running on http://127.0.0.1:3000");
